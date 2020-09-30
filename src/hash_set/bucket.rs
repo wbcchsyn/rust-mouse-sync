e2f8fd50c64a073d8477bfd6f39e53111a204202
Yoshida Shin <wbcchsyn@gmail.com>
2020-09-30 13:41:28 +0900

    Create module hash_set::bucket

--- src/hash_set/bucket.rs
+++ src/hash_set/bucket.rs
@@ -28,8 +28,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-//! `hash_set` provides implementations of thread-safe hash set.
-
-mod bucket;
-mod node;
