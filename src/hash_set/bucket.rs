107a3e52ba03f0c5f31079d280b86d37ca399888
Yoshida Shin <wbcchsyn@gmail.com>
2020-09-30 13:43:10 +0900

    Create struct Bucket

--- src/hash_set/bucket.rs
+++ src/hash_set/bucket.rs
@@ -28,3 +28,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use super::node::Node;
+
+/// Bucket for "chaining hash set".
+///
+/// This struct forms forward linked list by itself.
+pub struct Bucket<T>(*mut Node<T>);
