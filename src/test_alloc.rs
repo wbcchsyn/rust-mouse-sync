557672f188834a504c47bb35bf68551a58f6dec7
Yoshida Shin <wbcchsyn@gmail.com>
2020-10-21 16:49:48 +0900

    Create module test_alloc for test

--- src/test_alloc.rs
+++ src/test_alloc.rs
@@ -28,13 +28,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![deny(missing_docs)]
-
-//! # mouse-sync
-
-pub mod mutex;
-#[cfg(test)]
-mod test_alloc;
-
-pub use mutex::*;
